4bc2f0c76eb1c4f989b031afcdb5dfe2bb2cb688
rochebox <rochebox@gmail.com>
2016-08-16 15:37:07 +0000

    Lesson 13 course input validations

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 
 gem 'devise', '>= 3.2.4'
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Flixter::Application.routes.draw do
   devise_for :users
+
   #ADDED THIS ROUTE TO INDEX IN LESSON 4
   root 'static_pages#index'
+
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
