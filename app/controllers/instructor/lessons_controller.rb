c1ff5aee7f748ef806325c65392734c9a6815645
Christopher Roche <christopherroche@stmarksschool.org>
2016-08-17 12:41:58 -0400

    Lesson 17 Helper Methods Refactoring and before_actionsgit add --all

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,16 +1,33 @@
 class Instructor::LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_section
+
 	def new
-	   @section = Section.find(params[:section_id])
 	   @lesson = Lesson.new
 	end
 
 	def create
-	   @section = Section.find(params[:section_id])
-	   @lesson = @section.lessons.create(lesson_params)
-	   redirect_to instructor_course_path(@section.course)
+	   @lesson = current_section.lessons.create(lesson_params)
+	   redirect_to instructor_course_path(current_section.course)
 	end
 
 	private
+
+	#This is for the before_action
+	
+	def require_authorized_for_current_section
+		if current_section.course.user != current_user
+	      return render text: 'AHHHH This is unauthorized!!!!! No, No, No!!!', status: :unauthorized
+	   end
+	end
+
+	# This does memoization--remembers the last value looked up
+	# And to make it callable from our views you have to make it a helper_method..
+
+	helper_method :current_section
+  	def current_section
+   	 @current_section ||= Section.find(params[:section_id])
+ 	end
 
 	def lesson_params
 	   params.require(:lesson).permit(:title, :subtitle)
