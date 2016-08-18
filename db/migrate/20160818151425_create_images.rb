78546057e7e777df0dcc55a5c024cf05d24b0463
rochebox <rochebox@gmail.com>
2016-08-18 15:54:00 +0000

    Lesson 21 stage 1 adding model for image

---
+++ db/migrate/20160818151425_create_images.rb
@@ -0,0 +1,13 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+      t.text :caption
+      t.integer :course_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+
+    add_index :images, :course_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817140224) do
+ActiveRecord::Schema.define(version: 20160818153045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20160817140224) do
   end
 
   add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
+
+  create_table "images", force: true do |t|
+    t.text     "caption"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "images", ["course_id"], name: "index_images_on_course_id", using: :btree
 
   create_table "lessons", force: true do |t|
     t.string   "title"
