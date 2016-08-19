42a689552bdbe43ec5b40ba542f14e973fd7a3bc
rochebox <rochebox@gmail.com>
2016-08-19 22:51:02 +0000

    video uploading lesson 23 works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818204655) do
+ActiveRecord::Schema.define(version: 20160819221326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160818204655) do
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video"
   end
 
   add_index "lessons", ["section_id"], name: "index_lessons_on_section_id", using: :btree
