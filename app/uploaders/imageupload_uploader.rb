2eb46c9ebcf054c9071a4af5bb247e421030f865
rochebox <rochebox@gmail.com>
2016-08-21 19:21:50 +0000

    just added a link for the video-js nothing more

--- app/uploaders/imageupload_uploader.rb
+++ app/uploaders/imageupload_uploader.rb
@@ -28,7 +28,7 @@ class ImageuploadUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-   process resize_to_fill: [800,350]
+   process resize_to_fill: [800,375]
 
   # def scale(width, height)
   #   # do something
