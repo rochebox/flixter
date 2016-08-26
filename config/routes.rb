c80e3996ae367a530e70c1639c83b9b4fb96afa8
rochebox <rochebox@gmail.com>
2016-08-26 17:36:09 +0000

    Lesson 33 adding teacher learning dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
+
   root 'static_pages#index'
-  
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
