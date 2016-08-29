07fecab2e6717b33a259e513121d2908d302127c
rochebox <rochebox@gmail.com>
2016-08-29 05:34:31 +0000

    styling for dashboard for final

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,16 @@ class StaticPagesController < ApplicationController
 	#I made this in Lesson 4
 	def index
 	end
+
+	#I made this in Lesson 38
+	def privacy
+	end
+
+	#I made this in Lesson 38
+	def team
+	end
+
+	#I made this in Lesson 38
+	def career
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Flixter::Application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
-
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'career', to: 'static_pages#career'
+  
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
